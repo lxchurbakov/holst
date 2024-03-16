f7c1dd3fc66f5381b91d194f76721cfd7450e4c2
lxchurbakov <lxchurbakov@gmail.com>
2024-03-16 12:40:55 +0300

    Add parcel built website to test holst

---
+++ test/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { createRoot } from 'react-dom';
+
+import Holst from '../lib';
+
+const app = document.getElementById('app');
+
+const root = createRoot(app);
+
+root.render(<Holst />);
+
