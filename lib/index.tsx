af09cf4c35a191feca41efa23e37c295a58961bf
lxchurbakov <lxchurbakov@gmail.com>
2024-03-18 11:44:31 +0300

    Make basic translate

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,7 +1,55 @@
 import React from 'react';
 
-export default () => {
+import s from './index.module.css';
+
+export default ({ children }) => {
+    const wrapRef = React.useRef(null);
+    const containerRef = React.useRef(null);
+
+    React.useEffect(() => {
+        const wrap = wrapRef.current;
+        const container = containerRef.current;
+
+        if (!wrap || !container) {
+            return;
+        }
+
+        const mousedown = ({ clientX: startx, clientY: starty }) => {
+            const mousemove = ({ clientX: x, clientY: y }) => {
+                const diffx = x - startx;
+                const diffy = y - starty;
+
+                container.style.transform = `translate(${diffx}px, ${diffy}px)`;    
+            };
+
+            const mouseup = ({ clientX: x, clientY: y }) => {
+                const diffx = x - startx;
+                const diffy = y - starty;
+
+                container.style.transform = `none`;
+                container.style.left = (parseInt(container.style.left) || 0) + diffx;
+                container.style.top = (parseInt(container.style.top) || 0) + diffy;
+
+                window.removeEventListener('mousemove', mousemove);
+                window.removeEventListener('mouseup', mouseup);
+            };
+
+            window.addEventListener('mousemove', mousemove);
+            window.addEventListener('mouseup', mouseup);
+        };
+
+        wrap.addEventListener('mousedown', mousedown);
+
+        return () => {
+            wrap.removeEventListenter('mousedown', mousedown);
+        };
+    }, []);
+
     return (
-        <div>Hellow</div>
+        <div className={s.wrap} ref={wrapRef}>
+            <div className={s.container} ref={containerRef}>
+                {children}
+            </div>
+        </div>
     );
 };
